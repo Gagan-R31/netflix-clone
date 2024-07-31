c4bebeeba7cdea4105d3c06e95d0172634bade4d
Sparsh Khandelwal <82154970+sparshk380@users.noreply.github.com>
2024-07-31 12:23:33 +0530

    Create k8sAgent.groovy

---
+++ vars/k8sAgent.groovy
@@ -0,0 +1,33 @@
+def call() {
+    return """
+    apiVersion: v1
+    kind: Pod
+    metadata:
+      labels:
+        some-label: some-label-value
+    spec:
+      containers:
+      - name: jnlp
+        image: jenkins/inbound-agent
+        args: ['$(JENKINS_SECRET)', '$(JENKINS_NAME)']
+      - name: kaniko
+        image: gaganr31/kaniko-go
+        command:
+        - /busybox/sh
+        tty: true
+        volumeMounts:
+        - name: kaniko-secret
+          mountPath: /kaniko/.docker
+        - name: workspace-volume
+          mountPath: /workspace
+      volumes:
+      - name: kaniko-secret
+        secret:
+          secretName: kaniko-secret
+          items:
+          - key: .dockerconfigjson
+            path: config.json
+      - name: workspace-volume
+        emptyDir: {}
+    """.stripIndent()
+}
